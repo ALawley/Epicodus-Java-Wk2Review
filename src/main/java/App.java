5cf571d60cde78694ec73a489eafd370aa711176
Anna Lawley <ALawley1@gmail.com>
2016-02-19 11:41:52 -0800

    add sentence form to front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -65,21 +65,47 @@ public class App {
     }, new VelocityTemplateEngine());
 
     post("/definition-new", (request,response) -> {
-    HashMap<String,Object> model = new HashMap<String,Object>();
+      HashMap<String,Object> model = new HashMap<String,Object>();
 
-    String meaning = request.queryParams("definition");
-    String part = request.queryParams("part");
-    String sentence = request.queryParams("sentence");
-    Sentence newSentence = new Sentence(sentence);
-    Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
-    Definition newDefinition = new Definition(meaning, part);
-    newDefinition.addSentence(newSentence);
-    Word.find(word.getId()).addDefinition(newDefinition);
-    model.put("word", word);
+      String meaning = request.queryParams("definition");
+      String part = request.queryParams("part");
+      String sentence = request.queryParams("sentence");
+      Sentence newSentence = new Sentence(sentence);
+      Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+      Definition newDefinition = new Definition(meaning, part);
+      newDefinition.addSentence(newSentence);
+      Word.find(word.getId()).addDefinition(newDefinition);
+      model.put("word", word);
 
-    model.put("template", "templates/word.vtl");
-    return new ModelAndView(model, layout);
-  }, new VelocityTemplateEngine());
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/:id/:id2/sentences/new", (request,response) -> {
+      HashMap<String,Object> model = new HashMap<String,Object>();
+
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      Definition definition =Definition.find(Integer.parseInt(request.params(":id2")));
+      model.put("word", word);
+      model.put("definition", definition);
+
+      model.put("template", "templates/sentence-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/sentence-new", (request,response) -> {
+      HashMap<String,Object> model = new HashMap<String,Object>();
+
+      String sentence = request.queryParams("sentence");
+      Sentence newSentence = new Sentence(sentence);
+      Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+      Definition definition = Definition.find(Integer.parseInt(request.queryParams("definitionId")));
+      Definition.find(definition.getId()).addSentence(newSentence);
+      model.put("word", word);
+
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
   }
